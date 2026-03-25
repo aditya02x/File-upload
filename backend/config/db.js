aa1afe68d2021781e4d0bd7f669e94a7e8d87cbc
Aditya Raj <adityaraj34@icloud.com>
2026-03-25 23:50:30 +0530

    db done

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDb = async ()=>{
+    try {
+        await mongoose.connect(process.env.Mon)
+        
+    } catch (error) {
+        
+    }
+}
+
